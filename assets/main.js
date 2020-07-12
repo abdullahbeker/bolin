1f1a2ddb28ec4c3da1822ba686b4c3ec0e4a7529
abdullahbeker <abdullahbeker.54@hotmail.com>
2020-07-12 21:26:34 +0300

    update ui for chatting and the server to handle bidirectional communication with socket.io

--- assets/main.js
+++ assets/main.js
@@ -8,4 +8,20 @@ const handleNickSubmit = e => {
   $('#overlay').fadeOut(400)
   $('#content__send__input').focus()
 }
+const handleMessageSend = e => {
+  e.preventDefault()
+  console.log('hi')
+  let input = $('#content__send__input')
+  socket.emit('newMessage', { who: nickname, what: input.val() })
+  input.val('')
+}
+const addMessage = data => {
+  $('#content__screen__messages').append(`
+    <li class="content__screen__message"> ${data.who} says ${data.what}</li>
+  `)
+}
+socket.on('newMessage', data => {
+  addMessage(data)
+})
 $('#overlay__form').on('submit', handleNickSubmit)
+$('#content__send__form').on('submit', handleMessageSend)
--- server.js
+++ server.js
@@ -4,7 +4,16 @@ const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 const path = require('path')
 
-io.on('connection', client => {})
+io.on('connection', client => {
+  client.on('newMessage', data => {
+    data.at = new Date().toLocaleString()
+    io.sockets.emit('newMessage', data)
+  })
+
+  setInterval(() => {
+    client.emit('newMessage', { who: 'Server', what: `current server time is ${new Date().toLocaleString()}`, at: new Date().toLocaleString() })
+  }, 5000)
+})
 
 app.use(express.static(path.resolve(__dirname, 'assets', 'dist')))
 
